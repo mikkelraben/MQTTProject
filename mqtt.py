a83483d785ef1a2062776cf6d527d0319bf14b4d
Markus Friis Hansen <mfh2409@gmail.com>
2021-12-06 13:00:52 +0100

    Lavede funktioner til forskellige events

--- mqtt.py
+++ mqtt.py
@@ -1,4 +1,5 @@
 #Backend
+from ctypes import c_longdouble
 import paho
 import paho.mqtt.client as mqtt
 
@@ -12,12 +13,29 @@ Client_id = f"MikkelMarkus"
 def on_connect(client,obj,flags,rc):
     print(f"Oprettede forbindelse {str(rc)}")
 
+def on_sub(client,userdata,mid):
+    print()
+
+def on_publish(client, userdata, mid):
+    print()
+
+def on_message(client, userdata, msg):
+    print()
+
 
 client = mqtt.Client()
 client.username_pw_set(Username,Password)
 client.on_connect = on_connect
+client.on_subscribe = on_sub
+client.on_publish = on_publish
+client.on_message = on_message
 client.connect(ip,port,60)
 
-client.loop_forever()
+
+rc = 0
+while rc == 0:
+    rc = client.loop()
+
+
 
 
