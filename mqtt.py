d6d993aa0e32c1dc708649582702ef3dd4d77d22
Markus Friis Hansen <mfh2409@gmail.com>
2021-12-06 12:37:15 +0100

    Oprettede forbindelse til mqtt server

--- mqtt.py
+++ mqtt.py
@@ -5,6 +5,19 @@ import paho.mqtt.client as mqtt
 ip = "85.184.168.11"
 port = 1883
 
-Username = "19xsz"
+Username = "19xvz"
 Password = "msg007"
+Client_id = f"MikkelMarkus"
+
+def on_connect(client,obj,flags,rc):
+    print(f"Oprettede forbindelse {str(rc)}")
+
+
+client = mqtt.Client()
+client.username_pw_set(Username,Password)
+client.on_connect = on_connect
+client.connect(ip,port,60)
+
+client.loop_forever()
+
 
