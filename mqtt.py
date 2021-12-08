455307c1da1ff84cbad3d827c9e8347ea212f6fd
Markus Friis Hansen <mfh2409@gmail.com>
2021-12-08 10:03:30 +0100

    finished event functions

--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,4 @@
 #Backend
-from ctypes import c_longdouble
 import paho
 import paho.mqtt.client as mqtt
 
@@ -8,28 +7,43 @@ port = 1883
 
 Username = "19xvz"
 Password = "msg007"
-Client_id = f"MikkelMarkus"
+Client_id = f"Mikkel&Markus"
+
+#Event functions
 
 def on_connect(client,obj,flags,rc):
-    print(f"Oprettede forbindelse {str(rc)}")
+    print(f"Oprettede forbindelse med koden: {str(rc)}")
 
-def on_sub(client,userdata,mid):
-    print(f"")
+def on_subscribe(client,userdata,mid,qos):
+    print(f"You have subscribed to {str(mid)}")
+
+def on_unsubscribe(client,userdata,mid):
+    print(f"You have unsubscribed to {str(mid)}")
 
 def on_publish(client, userdata, mid):
-    print(f"")
+    print(f"Your message has been published to {str(mid)}")
 
 def on_message(client, userdata, msg):
-    print(f"{msg.topic}: {msg.payload}")
+    print(f"{msg.topic}: {str(msg.payload)}")
+
+
+#Functions
 
 
 client = mqtt.Client()
+
 client.username_pw_set(Username,Password)
+
+#Assign the events to our event functions
 client.on_connect = on_connect
-client.on_subscribe = on_sub
+client.on_subscribe = on_subscribe
+client.on_unsubscribe = on_unsubscribe
 client.on_publish = on_publish
 client.on_message = on_message
+
+
 client.connect(ip,port,60)
+
 
 
 rc = 0
